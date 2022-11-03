61e3dbc6559614af8b433599adeb8fca402ffea2
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-11-03 16:55:14 +0100

    Adds solutin for SortedArraySquares

---
+++ src/main/java/net/ivanzykov/twopointers/SortedArraySquares.java
@@ -0,0 +1,31 @@
+package net.ivanzykov.twopointers;
+
+class SortedArraySquares {
+
+    public static int[] makeSquares(int[] arr) {
+        int[] squares = new int[arr.length];
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        int i = right;
+
+        while (left < right) {
+            int squareLeft = arr[left] * arr[left];
+            int squareRight = arr[right] * arr[right];
+
+            if (squareLeft > squareRight) {
+                squares[i] = squareLeft;
+                left++;
+                i--;
+            } else {
+                squares[i] = squareRight;
+                right--;
+                i--;
+            }
+        }
+
+        return squares;
+    }
+}
+
